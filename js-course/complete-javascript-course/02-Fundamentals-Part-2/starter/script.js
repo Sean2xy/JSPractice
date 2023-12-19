12631beb19ea1dc91317c8b62a122b6bb42be9b0
10699 <xiaoyaoy103@gmail.com>
2023-12-19 23:29:53 +0000

    update

--- js-course/complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
+++ js-course/complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
@@ -90,22 +90,90 @@
 // }
 // console.log(yearLeftRetire(3,5));
 
-const calcAverage = (score1,score2,score3) =>{
-    const avgS = (score1+score2+score3) / 3;
-    return avgS;
-}
-const teamD = calcAverage(44,23,71);
-const teamK = calcAverage(65,54,49);
-const checkWinner = function (teamD,teamK) {
-    if(teamD >= 2 * teamK){
-        console.log(`teamD wins (${teamD} vs ${teamK})`);
-    }else if(teamK >= 2 * teamD){
-        console.log(`teamK wins (${teamK} vs ${teamD})`)
-    }else if(teamK === teamD){
-        console.log(`draw`)
-    }else{
-        console.log('nobody wins');
-    }
-}
-const win = checkWinner(teamD,teamK);
-console.log(win);
+// const calcAverage = (score1,score2,score3) =>{
+//     const avgS = (score1+score2+score3) / 3;
+//     return avgS;
+// }
+// const teamD = calcAverage(44,23,71);
+// const teamK = calcAverage(5,5,49);
+// const checkWinner = function (teamD,teamK) {
+//     if(teamD >= 2 * teamK){
+//         console.log(`teamD wins (${teamD} vs ${teamK})`);
+//     }else if(teamK >= 2 * teamD){
+//         console.log(`teamK wins (${teamK} vs ${teamD})`)
+//     }else if(teamK === teamD){
+//         console.log(`draw`)
+//     }else{
+//         console.log('nobody wins');
+//     }
+// }
+// const win = checkWinner(teamD,teamK);
+// console.log(win);
+
+// const friends = ['ss','zz','cc'];
+// const years = new Array(11,22,33,44);
+// console.log(years);
+// console.log(friends[0]);
+// console.log(friends.length);
+// friends[0]='jay';
+// console.log(friends[0]);
+//
+// const job = 'coder';
+// const sean = [job,'sean',11,2035-1991,friends];
+// console.log(sean);
+//
+// //ex
+// const calAge = function (birthYear) {
+//     return 2037-birthYear;
+// }
+// const calAge1 = calAge(years[0]);
+// console.log(calAge1);
+
+// array methods
+// const friends = ['ss','zz','cc'];
+// friends.push('jay');
+// const newLength = friends.push('jay');
+// console.log(friends);
+// console.log(newLength);
+// friends.unshift('sonn');
+//
+// friends.pop();
+// console.log(friends);
+//
+// friends.shift();
+// console.log(friends);
+//
+// console.log(friends.indexOf('jay'));
+// console.log(friends.includes('jay'));//strict equal
+//
+// if(friends.includes('jay')){
+//     console.log('oh lala');
+// }
+
+// code challenge 2
+
+// const calcTip = function (bill) {
+//     const percent = (bill>=50 && bill<=300)?0.15:0.2;
+//     const tip = bill * percent;
+//     console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${tip+bill}`);
+//     return tip;
+// }
+// const arrays=[125,555,44];
+// const tip1 = calcTip(arrays[0]);
+// const tip2 = calcTip(arrays[1]);
+// const tip3 = calcTip(arrays[2]);
+// const totals = [arrays[0]+tip1,arrays[1]+tip2,arrays[2]+tip3];
+// console.log(totals);
+
+//object
+// order of properties does not matter
+const sean = {
+    firstName: 'sean',
+    age:2027-2011,
+    job:'coder',
+    firends:['se','a','n']
+};
+console.log(sean);
+console.log(sean.firstName);
+console.log(sean['firstName']);
+
