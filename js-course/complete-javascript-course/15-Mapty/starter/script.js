8ae8aceafc989bf96690a97b3920b9ab11cdfa1e
10699 <xiaoyaoy103@gmail.com>
2023-12-28 22:09:04 +0000

    update

--- js-course/complete-javascript-course/15-Mapty/starter/script.js
+++ js-course/complete-javascript-course/15-Mapty/starter/script.js
@@ -10,3 +10,12 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(function(p){
+    const {latitude} = p.coords;
+    const {longitude} = p.coords;
+    console.log(latitude,longitude);
+  },function() {
+    alert('could not get');
+  })
+}
