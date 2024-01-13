858ad972648662652c6f54bd81a9723488ae02a7
10699 <xiaoyaoy103@gmail.com>
2024-01-13 17:01:25 +0000

    update

--- js-course/complete-javascript-course/18-forkify/starter/src/js/config.js
+++ js-course/complete-javascript-course/18-forkify/starter/src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '60bc3438-5c1e-4b6a-aecd-675fc56abe9b';
-export const MODAL_CLOSE_SEC = 2.5;
+export const MODAL_CLOSE_SEC = 2;
